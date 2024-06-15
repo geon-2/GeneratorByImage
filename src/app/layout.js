86a6d59de7d3acfdd17de97e1ff8f2fe6e26e8ce
geon-2 <gunle0416@gmail.com>
2024-06-15 15:09:37 +0900

    config: add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,21 @@
-import MetaHead from '../components/MetaHead';
 import '../styles/global.css';
+
+export const metadata = {
+  title: "Auto Meme Generator",
+  description: "Create your own memes with this simple meme generator.",
+  googleAdsenseAccount: "ca-pub-7847773430575106",
+  viewport: "width=device-width, initial-scale=1",
+  openGraph: {
+    title: "Auto Meme Generator",
+    description: "Create your own memes with this simple meme generator.",
+    url: "https://www.memegenerator.kr/",
+    type: "website",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <MetaHead />
       <body>
         {children}
       </body>
