bbf289f60990f16956adab874a55482730d073a5
geon-2 <gunle0416@gmail.com>
2024-06-15 15:01:13 +0900

    config: add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,10 @@
-import Link from "next/link";
+import MetaHead from '../components/MetaHead';
 import '../styles/global.css';
-
-export const metadata = {
-  title: "Custom Meme Generator",
-  description: "Create your own memes with this simple meme generator.",
-  'google-adsense-account': 'ca-pub-7847773430575106'
-};
 
 export default function RootLayout({ children }) {
   return (
     <html>
+      <MetaHead />
       <body>
         {children}
       </body>
